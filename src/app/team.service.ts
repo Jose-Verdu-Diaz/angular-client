4a6d44eb50ee29737555976f636da55b9abdb89f
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-11-04 02:17:19 +0100

    http service

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Team } from './model/team';
 import { TEAMS } from '../db-data';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,28 @@ export class TeamService {
 
     }
 
+  /** GET teams from the server */
   getTeams(): Observable<Team[]> {
     return this.http.get<Team[]>(this.teamsUrl)
+      .pipe(
+        catchError(this.handleError<Team[]>('getTeams', []))
+      );
+  }
+
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
